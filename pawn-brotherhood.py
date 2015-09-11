b22314a087602e9c1fad8e8f3a735acceb519e31
ngAtesz <attila.molnar@codecool.com>
2015-09-11 09:10:54 +0200

    home island pawn brotherhood exercise

--- pawn-brotherhood.py
+++ pawn-brotherhood.py
@@ -1,8 +1,26 @@
-from string import ascii_lowercase as asc
+
+def get_save_pawns(pawn):
+    cols = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    pawn_col = pawn[0]
+    pawn_row = pawn[1]
+    safe_row = str(int(pawn_row)-1)
+
+    left_safe = cols[cols.index(pawn_col)+1] + safe_row if pawn_col is not "h" else ""
+    right_safe = cols[cols.index(pawn_col)-1] + safe_row if pawn_col is not "a" else ""
+
+    return left_safe, right_safe
+
 
 def safe_pawns(pawns):
-    chess_table = [[0 for i in range(0, 9)] for c in asc]
-    return 0
+    #safe2 = [pawn for pawn in pawns if get_save_pawns(pawn)[0] in pawns or get_save_pawns(pawn)[1] in pawns]
+
+    safe = []
+    for pawn in pawns:
+        left, right = get_save_pawns(pawn)
+        if left in pawns or right in pawns:
+            safe.append(pawn)
+
+    return len(safe)
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
