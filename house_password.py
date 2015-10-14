daf88ba65b5fd498c1686ea86c99fede4fe6b420
ngAtesz <attila.molnar@codecool.com>
2015-10-14 09:37:15 +0200

    Earlier exercises

---
+++ house_password.py
@@ -0,0 +1,17 @@
+import re
+
+def checkio(data):
+    return len(data) > 9 and re.match("(.*)[0-9]+(.*)", data) and re.match("(.*)[a-z]+(.*)", data) and re.match("(.*)[A-Z]+(.*)", data)
+
+#Some hints
+#Just check all conditions
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
