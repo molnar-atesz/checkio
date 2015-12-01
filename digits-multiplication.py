36adf2fec24e7695f492833a8c93d7ac070b38d4
molnar45 <molnar45@hotmail.com>
2015-12-02 00:01:10 +0100

    digit multiplication slution before sleep

---
+++ digits-multiplication.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+
+def checkio(number):
+    return reduce(lambda x,y: x*y, [int(num) for num in str(number) if num != "0"])
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
