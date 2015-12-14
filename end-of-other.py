fbb9c730c535ac948b53204e1345d6a8aa480d7b
ngAtesz <molnar45@hotmail.com>
2015-12-14 22:50:26 +0100

    End of other exercise solution.

---
+++ end-of-other.py
@@ -0,0 +1,17 @@
+def checkio(words_set):
+    for word in words_set:
+        diff_set = words_set - {word}
+
+        for other_word in diff_set:
+            if other_word.endswith(word):
+                return True
+
+    return False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
